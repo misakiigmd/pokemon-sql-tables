3ef9524bad0379b63373dff0bc198e010d3bede6
misaki. <misakiigmd@gmail.com>
2023-11-15 09:17:34 +0100

    Created first Table

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import requests
+import sqlite3
 
 r = requests.get('https://pokebuildapi.fr/api/v1/pokemon')
 data = r.json()
+
+db = sqlite3.connect('pokemon.db')
+cursor = db.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Pokemon(
+        pokemon_id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        atk INTEGER,
+        def INTEGER,
+        hp INTEGER
+    );
+    ''')
