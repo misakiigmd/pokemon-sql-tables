ad4490796a44500c14c43d715a252f7bc037cc4e
misaki. <misakiigmd@gmail.com>
2023-11-15 09:20:37 +0100

    Added Trainer table

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlite3
 r = requests.get('https://pokebuildapi.fr/api/v1/pokemon')
 data = r.json()
 
-db = sqlite3.connect('pokemon.db')
+db = sqlite3.connect('pokemons.db')
 cursor = db.cursor()
 
 cursor.execute('''
@@ -14,5 +14,24 @@ cursor.execute('''
         atk INTEGER,
         def INTEGER,
         hp INTEGER
+    );
+    ''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Trainer(
+        trainer_id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        pokemon1 INTEGER,
+        pokemon2 INTEGER,
+        pokemon3 INTEGER,
+        pokemon4 INTEGER,
+        pokemon5 INTEGER,
+        pokemon6 INTEGER,
+        FOREIGN KEY(pokemon1) REFERENCES Pokemon(pokemon_id),
+        FOREIGN KEY(pokemon2) REFERENCES Pokemon(pokemon_id),
+        FOREIGN KEY(pokemon3) REFERENCES Pokemon(pokemon_id),
+        FOREIGN KEY(pokemon4) REFERENCES Pokemon(pokemon_id),
+        FOREIGN KEY(pokemon5) REFERENCES Pokemon(pokemon_id),
+        FOREIGN KEY(pokemon6) REFERENCES Pokemon(pokemon_id)
     );
     ''')
