d10a56f860f60caa867bef9e764a9921b3a18eeb
misaki. <misakiigmd@gmail.com>
2023-11-17 10:48:51 +0100

    Added comments

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from random import randint
 import requests
 import sqlite3
 
+# On utilise une API pour récupérer les données des pokémons
 r = requests.get('https://pokebuildapi.fr/api/v1/pokemon')
 data = r.json()
 
+# On crée deux tables dans la base de données, une pour les pokémons et une pour les dresseurs
 db = sqlite3.connect('pokemons.db')
 cursor = db.cursor()
 
@@ -12,11 +14,11 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS Pokemon(
         pokemon_id INTEGER PRIMARY KEY,
         name VARCHAR(255),
-        atk INTEGER,
-        def INTEGER,
+        attack INTEGER,
+        defense INTEGER,
         hp INTEGER,
-        sp_atk INTEGER,
-        sp_def INTEGER,
+        special_attack INTEGER,
+        special_defense INTEGER,
         speed INTEGER,
         type1 VARCHAR(255),
         type2 VARCHAR(255),
@@ -46,14 +48,16 @@ cursor.execute('''
     );
     ''')
 
+# On insère les données des pokémons dans la base de données
 for pokemon in data:
     evo = pokemon['apiEvolutions'][0]['pokedexId'] if len(pokemon['apiEvolutions']) == 1 else None
     pre_evo = pokemon['apiPreEvolution']['pokedexIdd'] if len(pokemon['apiPreEvolution']) == 2 else None 
     cursor.execute(f'''
-        INSERT INTO Pokemon(pokemon_id, name, atk, def, hp, sp_atk, sp_def, speed, type1, type2, evolution, pre_evolution)
+        INSERT INTO Pokemon(pokemon_id, name, attack, defense, hp, special_attack, special_defense, speed, type1, type2, evolution, pre_evolution)
         VALUES(? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,?);
         ''', (pokemon['id'], pokemon['name'], pokemon['stats']['attack'], pokemon['stats']['defense'], pokemon['stats']['HP'], pokemon['stats']['special_attack'], pokemon['stats']['special_defense'], pokemon['stats']['speed'], pokemon['apiTypes'][0]['name'], pokemon['apiTypes'][1]['name'] if len(pokemon['apiTypes']) > 1 else None, evo, pre_evo))
     
+# On crée les données des dresseurs manuellement, avec des pokémons aléatoires et on les insère dans la base de données
 trainer_list = [
     {'name': 'Ash', 'pokemon1': randint(1, 898), 'pokemon2': randint(1, 898), 'pokemon3': randint(1, 898), 'pokemon4': randint(1, 898), 'pokemon5': randint(1, 898), 'pokemon6': randint(1, 898)},
     {'name': 'Blue', 'pokemon1': randint(1, 898), 'pokemon2': randint(1, 898), 'pokemon3': randint(1, 898), 'pokemon4': randint(1, 898), 'pokemon5': randint(1, 898), 'pokemon6': randint(1, 898)},
@@ -69,12 +73,15 @@ for trainer in trainer_list:
         VALUES(? ,? ,? ,? ,? ,? ,?);
         ''', (trainer['name'], trainer['pokemon1'], trainer['pokemon2'], trainer['pokemon3'], trainer['pokemon4'], trainer['pokemon5'], trainer['pokemon6']))
 
+# On enregistre les changements dans la base de données
 db.commit()
     
+# Fonction pour récupérer un pokémon à partir de son ID
 def get_pokemon_by_id(pokemon_id):
     cursor.execute('SELECT * FROM Pokemon WHERE pokemon_id = ?', (pokemon_id,))
     return cursor.fetchone()
 
+# Fonction pour chercher un pokémon dans la base de données grâce à son nom
 def search_pokemon():
     query = input("Enter a pokémon to search for: ")
     result = cursor.execute(f"SELECT * FROM Pokemon WHERE name LIKE '%{query}%'")
@@ -105,12 +112,14 @@ def search_pokemon():
     Pre-evolution: {pre_evo[1] if pre_evo else None}
     ''')
 
+# Fonction pour récupérer un dresseur à partir de son ID
 def get_trainer_by_id(query):
     cursor.execute(f'''
         SELECT * FROM Trainer WHERE trainer_id = {query};
         ''')
     return list(cursor.fetchone())
 
+# Fonction pour chercher un dresseur dans la base de données grâce à son nom
 def search_trainer(): 
     query = input('Enter a trainer: ')
     result = cursor.execute(f"SELECT * FROM trainer WHERE name LIKE '%{query}%'")
@@ -135,6 +144,7 @@ def search_trainer():
     Pokémon 6: {get_pokemon_by_id(result[7])[1]}
     """)
     
+# Fonction pour créer un nouveau dresseur
 def new_trainer():
     name = input("Enter your name: ")
     pokemons = [randint(1, 898) for i in range(6)]
@@ -143,7 +153,9 @@ def new_trainer():
         VALUES(? ,? ,? ,? ,? ,? ,?);
         ''', (name, pokemons[0], pokemons[1], pokemons[2], pokemons[3], pokemons[4], pokemons[5]))
     db.commit()
+    print("Trainer created.")
     
+# Fonction pour chercher un pokémon ou un dresseur
 def search():
     query = input("What do you want to search for? (pokemon/trainer): ")
     if query.lower() == "pokemon" or query.lower() == "pokémon":
@@ -154,16 +166,37 @@ def search():
         print("Invalid input.")
         search()
         
+# Formule pour calculer les dégâts pendant les combats
 def calculate_damage(attacker, defender):
     damage = (50 * attacker[2] * randint(1, 101) / 100) / defender[3]
     return int(damage)
 
+# Fonction pour lister les dresseurs
+def list_trainers():
+    cursor.execute('SELECT * FROM Trainer')
+    trainers = cursor.fetchall()
+    print("\nTrainers:")
+    for trainer in trainers:
+        print(f"{trainer[0]}: {trainer[1]}")
+    
+# Fonction pour lister les pokémons d'un dresseur
+def list_pokemons_by_trainer(trainer_id):
+    trainer = get_trainer_by_id(trainer_id)
+    pokemons = [get_pokemon_by_id(trainer[2]), get_pokemon_by_id(trainer[3]), get_pokemon_by_id(trainer[4]), get_pokemon_by_id(trainer[5]), get_pokemon_by_id(trainer[6]), get_pokemon_by_id(trainer[7])]
+    print("\nPokémons:")
+    for pokemon in pokemons:
+        print(f"{pokemon[0]}: {pokemon[1]}")
+        
+# Fonction pour simuler un combat entre deux dresseurs et leurs pokémons
 def fight():
+    list_trainers()
     trainer1 = get_trainer_by_id(int(input("Enter the ID of the first trainer: ")))
     trainer2 = get_trainer_by_id(int(input("Enter the ID of the second trainer: ")))
-    pokemon1 = get_pokemon_by_id(trainer1[2])
-    pokemon2 = get_pokemon_by_id(trainer2[2])
-    print(f"{trainer1[1]} VS {trainer2[1]}")
+    list_pokemons_by_trainer(trainer1[0])
+    pokemon1 = get_pokemon_by_id(int(input("Enter the ID of the first pokemon: ")))
+    list_pokemons_by_trainer(trainer2[0])
+    pokemon2 = get_pokemon_by_id(int(input("Enter the ID of the second pokemon: ")))
+    print(f"\n{trainer1[1]} VS {trainer2[1]}")
     while trainer1[2] > 0 and trainer2[2] > 0:
         print(f"{pokemon1[1]} attacks {pokemon2[1]}")
         damage = calculate_damage(get_pokemon_by_id(trainer1[2]), get_pokemon_by_id(trainer2[2]))
@@ -184,4 +217,18 @@ def fight():
     elif trainer2[2] > trainer1[2]:
         print(f"{trainer2[1]} wins!")
         
-fight()
+# Menu principal
+if __name__ == '__main__':
+    while True:
+        print("\n1. Search\n2. New trainer\n3. Fight\n4. Exit\n")
+        choice = input("What do you want to do? (int): ")
+        if choice == '1':
+            search()
+        elif choice == '2':
+            new_trainer()
+        elif choice == '3':
+            fight()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid input.")
